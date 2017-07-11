47c89c509e4496e08c02e09e65abc943ddd0f7f6
Paul Bismuth <paul.bismuth.f@gmail.com>
2017-07-11 20:00:07 +0200

    Now walk the history

--- historiography.go
+++ historiography.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/backinmydays/historiography/utils"
 	"github.com/golang/glog"
+	"github.com/paul-bismuth/historiography/utils"
 	"github.com/spf13/cobra"
 	git "gopkg.in/libgit2/git2go.v25"
 )
@@ -11,17 +11,31 @@ var root = &cobra.Command{
 	Use:   "historiography",
 	Short: "Rewrite git history dates",
 	Run: func(cmd *cobra.Command, args []string) {
-		repos := []*git.Repository{}
-
 		for _, arg := range args {
 			if repo, err := git.OpenRepository(arg); err != nil {
 				utils.MustG(err)
 			} else {
-				repos = append(repos, repo)
+				defer repo.Free()
+				WalkRepo(repo)
 			}
 		}
-		glog.Infof("%q", repos)
 	},
+}
+
+func IteratorFunc(commit *git.Commit) bool {
+	glog.Infof(commit.Summary())
+	return true
+}
+
+func WalkRepo(repo *git.Repository) {
+	if rev, err := repo.Walk(); err != nil {
+		utils.MustG(err)
+	} else {
+		defer rev.Free()
+		rev.Sorting(git.SortTime)
+		utils.MustG(rev.PushHead())
+		utils.MustG(rev.Iterate(IteratorFunc))
+	}
 }
 
 func init() {
