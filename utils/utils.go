fbc53ca5c4f44b833044317a19bb06c4de78f6e5
Paul Bismuth <paul.bismuth.f@gmail.com>
2017-07-12 23:06:43 +0200

    Change some implementation

--- utils/utils.go
+++ utils/utils.go
@@ -2,23 +2,40 @@ package utils
 
 import (
 	"github.com/golang/glog"
-	"log"
+	"math/rand"
+	"time"
 )
-
-func Must(err error) {
-	if err != nil {
-		log.Fatalf("aborting due to: %s", err)
-	}
-}
-
-func MustG(err error) {
-	if err != nil {
-		glog.Fatalf("aborting due to: %s", err)
-	}
-}
 
 func Maybe(err error) {
 	if err != nil {
 		glog.Errorf("%s", err)
 	}
+}
+
+// http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
+var src = rand.NewSource(time.Now().UnixNano())
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyz0123456789"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func SecureRandomString(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
